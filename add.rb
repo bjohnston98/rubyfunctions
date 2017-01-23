3f8b74e8f11dd7cb6f4a42961837621ac2bdfb22
Brian Johnston <b_johnston98@yahoo.com>
2017-01-23 10:05:11 -0500

    first func

---
+++ add.rb
@@ -0,0 +1,9 @@
+
+#def add	(1,1)		#add is the name
+#send #hard coded, need s variable
+
+def add(num1, num2)  # Do this!
+2		#we want to see two, so stick in
+
+end
+
---
+++ testadd.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'  #builit in term, you must start Method with test
+require_relative 'add.rb' # file linked to
+
+class TestMMFunctions < Minitest::Test #as b4
+	
+	def test_assert_equals_one()
+		assert_equal(1,1) 
+	end
+
+	def test_add_1_plus_1()
+		assert_equal(2, add(1,1))  #we want 2, equ on right. call Method Add, pass in 1,1
+
+	end
+
+end
+
